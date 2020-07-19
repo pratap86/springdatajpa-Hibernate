cded7badfb7350ccbcfaccce7d54550815f8a445
pratap86 <narayanpratap86@gmail.com>
2020-07-19 21:47:31 +0530

    H2 data base introduced

---
+++ springdatajpa/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO PRODUCT(id, name, description, price) values 
+(101, 'product1', 'Awesome', 2300),
+(102, 'product2', 'Good Product', 2350),
+(103, 'product3', 'Very good product', 2400),
+(104, 'product4', 'Awesome product', 2600);
+
+INSERT INTO EMPLOYEE(id, first_name, last_name) values
+(12345, 'test1', 'last1'),
+(12346, 'test12', 'last2'),
+(12347, 'test3', 'last3'),
+(12348, 'test4', 'last4');
---
+++ springdatajpa/resources/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE PRODUCT(
+id int AUTO_INCREMENT PRIMARY KEY,
+name varchar(20),
+description varchar(100),
+price decimal(8,3) 
+);
+
+CREATE TABLE EMPLOYEE(
+id int PRIMARY KEY,
+first_name varchar(20),
+last_name varchar(20)
+);
